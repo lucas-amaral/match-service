3f67bbc9a8258fcbc5a768ef26a9548ef34967c7
luke <amaral@devgrid.co.uk>
2020-12-07 23:57:59 -0300

    Fix send email notification call

--- src/main/java/com/proposta/aceita/matchservice/services/integrations/clients/NotificationClient.java
+++ src/main/java/com/proposta/aceita/matchservice/services/integrations/clients/NotificationClient.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "${services.notification.name}", url = "${services.notification.url}", decode404 = true)
 public interface NotificationClient {
 
-    @PostMapping("/emails/match-to-seller")
+    @PutMapping("/emails/match-to-seller")
     void sendMatchEmailForSeller(@RequestHeader("Authorization") String authHeader, @RequestBody Negotiation body);
 
-    @PostMapping("/emails/match-to-buyer")
+    @PutMapping("/emails/match-to-buyer")
     void sendMatchEmailForBuyer(@RequestHeader("Authorization") String authHeader, @RequestBody Negotiation body);
 
     @PutMapping("/deal")
